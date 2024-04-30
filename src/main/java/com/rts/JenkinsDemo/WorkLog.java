53f0280103cbbea487661e910e09c76c58d61f57
logesh karthick <logeshkarthick67@gmail.com>
2024-04-30 10:11:28 +0530

    First Commit

---
+++ src/main/java/com/rts/JenkinsDemo/WorkLog.java
@@ -0,0 +1,15 @@
+package com.rts.JenkinsDemo;
+
+
+public class WorkLog {
+		
+	public String addToLog(String name)
+	{
+		return "Added Log for " + name;
+	}
+	
+	public String addLogins (String loginName)
+	{
+		return "Added Login log for " + loginName;
+	}
+}
